f16d8564a73cfed76aa12812864107c81b9f5e7d
庞广龙 <377960738@163.com>
2026-06-07 16:31:37 +0800

    🎉 init(init): 首次提交

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM alpine:latest
+
+# 安装基础工具
+RUN apk add --no-cache \
+	tini \
+	docker.io \
+	docker-compose \
+	git \
+	curl \
+	wget \
+	vim \
+	openssh-server \
+	ca-certificates \
+	&& rm -rf /var/cache/apk/*
+
+# 暴露端口
+EXPOSE 22
+
+# 复制 entrypoint 脚本
+COPY entrypoint.sh /entrypoint.sh
+
+# 设置为入口点
+ENTRYPOINT ["/usr/bin/tini", "--", "bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# 准备 vscode-server 目录
+if [ -d /data ] && [ ! -d /data/.vscode-server ]; then
+  mkdir -p /data/.vscode-server /data/.vscode-remote-containers /data/.vscode-print-resource-cache
+
+  ln -s /data/.vscode-server /root/.vscode-server
+  ln -s /data/.vscode-remote-containers /root/.vscode-remote-containers
+  ln -s /data/.vscode-print-resource-cache /root/.vscode-print-resource-cache
+fi
+
+# 配置 SSH 服务
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^AllowTcpForwarding.*/AllowTcpForwarding yes/' /etc/ssh/sshd_config
+sed -i 's/^GatewayPorts.*/GatewayPorts yes/' /etc/ssh/sshd_config
+
+# 启动 SSH 服务
+/usr/sbin/sshd -D
+
+exec "$@"
